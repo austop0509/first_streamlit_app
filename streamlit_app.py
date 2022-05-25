e0e1fa2ed9785b044b223bdb7cb08e0959c7a747
austop0509 <106097267+austop0509@users.noreply.github.com>
2022-05-25 12:13:26 -0500

    Update streamlit_app.py
    Imported JSON data from Fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,14 @@ import requests
 		
 streamlit.title("My Parents New Healthy Diner")
 
+# displays favorite menu items
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
+# interactive menu for customers
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -17,5 +19,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+# show fruityvice API response
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
