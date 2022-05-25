c5e0a33b7da4cf62267d90a51a94dc00fe35a42c
austop0509 <106097267+austop0509@users.noreply.github.com>
2022-05-25 12:29:31 -0500

    Update streamlit_app.py
    Added user input to generate information about desired fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,9 @@ streamlit.dataframe(fruits_to_show)
 
 # show fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+fruit_choice = streamlit.text_input("What fruit would you like information about?", "kiwi")
+streamlit.write("The user entered", fruit_choice)
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 
 # normalize the JSON data
