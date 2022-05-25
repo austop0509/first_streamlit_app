5f02133110dec7ce7302abf90ebb6a7a600e6b73
austop0509 <106097267+austop0509@users.noreply.github.com>
2022-05-25 15:17:54 -0500

    Update streamlit_app.py
    Added query functionality using the Snowflake connector in the requirements.txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,13 @@ fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display normalized table
 streamlit.dataframe(fruityvice_normalized)
+
+# testing connection to Snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+# executes a query of the FRUIT_LOAD_LIST table and returns a data frame
+my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchall()
+streamlit.text("The fruit load list contains:")
+streamlit.dataframe(my_data_row)
